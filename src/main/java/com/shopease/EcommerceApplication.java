437296a51d27f2409cbba28eb05cedb8d07b1fe4
Bhavesh Shinde <shindebhavesh9947@gmail.com>
2026-04-02 22:00:07 +0530

    Initial commit

--- src/main/java/com/shopease/EcommerceApplication.java
+++ src/main/java/com/shopease/EcommerceApplication.java
@@ -1,5 +1,5 @@
-
 package com.shopease;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
